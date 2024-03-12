2a01129ee29a40bb4feab3da6cf89328b371a4b0
kevinniel <k.niel.pro@gmail.com>
2024-03-12 15:09:09 +0100

    update

--- src/Interfaces/InterfaceBank.php
+++ src/Interfaces/InterfaceBank.php
@@ -3,5 +3,12 @@
 namespace Interface;
 
 interface InterfaceBank {
-    public function giveMoney(int $amount): bool;
+    public function cashout(int $amount): bool;
+    public function cashin(int $amount): bool;
+    // public function giveConstruction(string $type): bool;
+    // public function takeConstruction(string $type): bool;
+    public function giveHouse(): bool;
+    public function takeHouse(): bool;
+    public function giveHotel(): bool;
+    public function takeHotel(): bool;
 }
